7014031ef3628a65eb154d44b2ca6eaf2fe355f4
Fawaz-Khan-R <115716567+Fawaz-Khan-R@users.noreply.github.com>
2024-03-14 12:21:09 +0530

    Update the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
     return(
         <div className="container">
             <div className="title">
-            Plan a Tour
+            Welcome to ExploreX
             </div>
         </div>
     )
